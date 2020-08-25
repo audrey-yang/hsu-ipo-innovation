b166295ae663d2350393b4a52259f215ee5d2c47
Kenneth Shinn (TEMP) <kennethshinn@jaysons-mbp-2.lan>
2020-08-25 14:19:48 -0700

    aggregated output included

--- kenneth's code/aggregate_generality_originality.py
+++ kenneth's code/aggregate_generality_originality.py
@@ -18,9 +18,14 @@ import time
 # start time
 start_time = time.ctime()
 
+print('READING FILES\n')
 # load in the ipo_match file
 og_file = open('dependent_data/originality_generality.csv', encoding='utf-8-sig')
 og = csv.DictReader(og_file, delimiter=",")
+
+# load in the firm year patent file
+firm_year_patentcnt_file = open('dependent_data/firm_year_patentcnt.csv', encoding='utf-8-sig')
+firm_year_patentcnt = csv.DictReader(firm_year_patentcnt_file, delimiter=",")
 
 # create an output file (firm year patent cnt)
 output = open('outputs/firm_year_innovation.csv', 'w', newline="\n", encoding='utf-8-sig')
@@ -31,6 +36,15 @@ firm_year_innovation.writerow(header)
 # create a dictionary of ipo_firms (firm, year) -> (originality_sum, generality_sum, cnt)
 innovation_dict = {}
 
+# find all the potential firm years
+print('CALCULATING ALL POTENTIAL FIRM YEAR\n')
+for row in firm_year_patentcnt:
+	firm = row['ipo_firm']
+	year = row['year']
+	firm_year = (firm, year)
+	innovation_dict[firm_year] = (0, 0, 0, 0)
+
+print('AGGREGATING ORIGINALITY AND GENERALITY SCORES\n')
 for row in og:
 	firm = row['ipo_firm']
 	year = row['year']
@@ -49,14 +63,16 @@ for row in og:
 			g_cnt += 1
 		innovation_dict[firm_year] = (originality_sum, generality_sum, o_cnt, g_cnt)
 	else:
-		if originality == 'N/A' and originality == 'N/A':
-			innovation_dict[firm_year] = (0, 0, 0, 0)
-		elif originality == 'N/A':
-			innovation_dict[firm_year] = (0, float(generality), 0, 1)
-		elif generality == 'N/A':
-			innovation_dict[firm_year] = (float(originality), 0, 1, 0)
-		else:
-			innovation_dict[firm_year] = (float(originality), float(generality), 1, 1)
+		print('ERROR FIRM YEAR NOT FOUND!!')
+		break
+		# if originality == 'N/A' and originality == 'N/A':
+		# 	innovation_dict[firm_year] = (0, 0, 0, 0)
+		# elif originality == 'N/A':
+		# 	innovation_dict[firm_year] = (0, float(generality), 0, 1)
+		# elif generality == 'N/A':
+		# 	innovation_dict[firm_year] = (float(originality), 0, 1, 0)
+		# else:
+		# 	innovation_dict[firm_year] = (float(originality), float(generality), 1, 1)
 
 for key, value in innovation_dict.items():
 	firm, year = key
@@ -81,4 +97,4 @@ print('\nEND OF PROCESS\n')
 end_time = time.ctime()
 
 print('Start Time: ' + start_time)
-print('End Time: ' + end_time)
+print('End Time: ' + end_time + '\n')
